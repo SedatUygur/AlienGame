217711ad4ea39c14df1eccfb589f70bb4e608400
Sedat Uygur <sedat.uygur@outlook.com>
2024-12-13 00:14:53 +0300

    🎉 move cannon by mouse position

--- aliengame/src/App.js
+++ aliengame/src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import Canvas from './components/Canvas';
+import { getCanvasPosition } from './utils/formulas';
 
-function App() {
-  return (
-    <Canvas />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.trackMouse = this.trackMouse.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+      self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
+  render() {
+    return (
+      <Canvas
+        angle={this.props.angle}
+        trackMouse={event => (this.trackMouse(event))}
+      />
+    );
+  }
 }
+
+App.propTypes = {
+  angle: PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired,
+};
 
 export default App;
