359cf97f41bac5ecb288771deab3d1f47e1c051e
Sedat Uygur <sedat.uygur@outlook.com>
2024-12-13 17:51:55 +0300

    🎉 add styled-components & move objects like flying

--- aliengame/src/reducers/moveObjects.js
+++ aliengame/src/reducers/moveObjects.js
@@ -2,17 +2,26 @@ import { calculateAngle } from '../utils/formulas';
 import createFlyingObjects from './createFlyingObjects';
 
 function moveObjects(state, action) {
+  const now = (new Date()).getTime();
   const mousePosition = action.mousePosition || {
     x: 0,
     y: 0,
   };
-  const newState = createFlyingObjects(state);
   const { x, y } = mousePosition;
+
+  const newState = createFlyingObjects(state);
   const angle = calculateAngle(0, 0, x, y);
+  const flyingObjects = newState.gameState.flyingObjects.filter(object => (
+    (now - object.createdAt) < 4000
+  ));
   
   return {
     ...newState,
     angle,
+    gameState: {
+      ...newState.gameState,
+      flyingObjects,
+    },
   };
 }
 
