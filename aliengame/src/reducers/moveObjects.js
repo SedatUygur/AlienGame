82c64b577e0616889d5ba4f8490b0e1bcf2a648d
Sedat Uygur <sedat.uygur@outlook.com>
2024-12-15 15:03:34 +0300

    🎮 update score by kills

--- aliengame/src/reducers/moveObjects.js
+++ aliengame/src/reducers/moveObjects.js
@@ -44,6 +44,8 @@ function moveObjects(state, action) {
 
   cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)));
   flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)));
+
+  const kills = state.gameState.kills + flyingDiscsDestroyed.length;
   
   return {
     ...newState,
@@ -52,6 +54,7 @@ function moveObjects(state, action) {
       ...newState.gameState,
       cannonBalls: [...cannonBalls],
       flyingObjects,
+      kills,
       lives,
       started,
     },
